5ad380c80c485ea2134b07f0f48a154fa760c166
hemingtai <heming_tai@163.com>
2017-03-02 17:32:40 +0800

    update code

--- HTNetworking/HTHTTPDownloadQueue.swift
+++ HTNetworking/HTHTTPDownloadQueue.swift
@@ -95,7 +95,38 @@ class HTHTTPDownloadQueue: NSObject
      */
     func addTask(withURLString urlString: String, progressHandler: HTProgressHandler?, completionHandler: HTURLDownloadCompletionHandler?)
     {
-        
+        if (![Utility isValidString:urlString]) {
+            return;
+        }
+        NSURL *url = [NSURL URLWithString:urlString];
+        if (url) {
+            NSURLSessionDownloadTask *downloadTask = [self.session downloadTaskWithURL:url];
+            
+            NSMutableDictionary *taskInfo = [NSMutableDictionary dictionary];
+            if (downloadTask) {
+                taskInfo[@"Task"] = downloadTask;
+            }
+            if (progressHandler) {
+                taskInfo[@"ProgressHandler"] = progressHandler;
+            }
+            if (completionHandler) {
+                taskInfo[@"CompletionHandler"] = completionHandler;
+            }
+            [_myLock lock];
+            self.tasks[@(urlString.hash).stringValue] = taskInfo;
+            [_myLock unlock];
+            // update progress
+            if (!self.queueProgress.isCancelled) {
+                self.queueProgress.totalUnitCount++;
+            }
+            if (self.queueProgressHandler) {
+                self.queueProgressHandler(self.queueProgress);
+            }
+            
+            [downloadTask resume];
+        } else {
+            completionHandler(nil, nil, nil);
+        }
     }
     
     /**
@@ -106,44 +137,160 @@ class HTHTTPDownloadQueue: NSObject
      */
     func addTask(withURLString urlString:String, completionHandler: HTURLDownloadCompletionHandler?)
     {
-        
+        [self addTaskWithURLString:urlString progressHandler:nil completionHandler:completionHandler];
     }
     
     //! 获取单个任务的进度
     func getTaskProgress(withURLString urlString: String) -> Progress
     {
-        
+        if (![Utility isValidString:urlString]) {
+            return nil;
+        }
+        NSString *key = @(urlString.hash).stringValue;
+        [_myLock lock];
+        NSURLSessionTask *task = self.tasks[key][@"Task"];
+        [_myLock unlock];
+        NSProgress *progress = [NSProgress progressWithTotalUnitCount:task.countOfBytesExpectedToReceive];
+        progress.completedUnitCount = task.countOfBytesReceived;
+        return progress;
     }
 
     //! 重设单个任务的进度回调
     func resetTaskProgressHandler(withURLString urlString: String, progressHandler: HTProgressHandler?)
     {
-        
+        if (![Utility isValidString:urlString]) {
+            return;
+        }
+        NSString *key = @(urlString.hash).stringValue;
+        [_myLock lock];
+        NSMutableDictionary *taskInfo = [self.tasks[key] mutableCopy];
+        if (progressHandler) {
+            taskInfo[@"ProgressHandler"] = progressHandler;
+        } else {
+            [taskInfo removeObjectForKey:@"ProgressHandler"];
+        }
+        self.tasks[key] = taskInfo;
+        [_myLock unlock];
     }
     
     //! 取消单个任务
     func cancelTask(withURLString urlString: String)
     {
-        
+        if (![Utility isValidString:urlString]) {
+            return;
+        }
+        NSString *key = @(urlString.hash).stringValue;
+        [_myLock lock];
+        NSURLSessionTask *task = self.tasks[key][@"Task"];
+        [task cancel];
+        [_myLock unlock];
     }
     //! 继续单个任务
     func resumeTask(withURLString urlString: String)
     {
-        
+        if (![Utility isValidString:urlString]) {
+            return;
+        }
+        [_myLock lock];
+        NSString *key = @(urlString.hash).stringValue;
+        NSURLSessionTask *task = self.tasks[key][@"Task"];
+        [task resume];
+        [_myLock unlock];
     }
     //! 挂起单个任务
     func suspendTask(withURLString urlString: String)
     {
-        
+        if (![Utility isValidString:urlString]) {
+            return;
+        }
+        [_myLock lock];
+        NSString *key = @(urlString.hash).stringValue;
+        NSURLSessionTask *task = self.tasks[key][@"Task"];
+        [task suspend];
+        [_myLock unlock];
     }
     //! 清空队列
     func clear()
     {
-    
+        [_myLock lock];
+        [self.tasks enumerateKeysAndObjectsUsingBlock:^(id key, NSDictionary *obj, BOOL *stop) {
+            [obj[@"Task"] cancel];
+            }];
+        [self.operationQueue cancelAllOperations];
+        [self.tasks removeAllObjects];
+        [_myLock unlock];
+        self.queueProgress.completedUnitCount = 0;
+        self.queueProgress.totalUnitCount = 0;
+        if (self.queueCompletionHandler) {
+            self.queueCompletionHandler();
+        }
     }
     //! 销毁队列，不能再添加任务
     func tearDown()
     {
-        
+        [self clear];
+        [self.session invalidateAndCancel];
+    }
+    
+    #pragma mark - NSURLSessionDelegate
+    - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
+    {
+    // didFinishDownloadingToURL已经回调完成了，所以此处无需处理完成，只要处理出错就可以了
+    if (error) {
+    NSString *key = @(task.originalRequest.URL.absoluteString.hash).stringValue;
+    [_myLock lock];
+    NSDictionary *taskInfo = self.tasks[key];
+    [_myLock unlock];
+    if (taskInfo) {
+    URLDownloadCompletionHandler handler = taskInfo[@"CompletionHandler"];
+    if (handler) {
+    handler(nil, task.response, error);
+    }
+    }
+    }
+    }
+    
+    #pragma mark - NSURLSessionDownloadDelegate
+    - (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location
+    {
+    NSString *key = @(downloadTask.originalRequest.URL.absoluteString.hash).stringValue;
+    [_myLock lock];
+    NSDictionary *taskInfo = self.tasks[key];
+    [_myLock unlock];
+    if (taskInfo) {
+    URLDownloadCompletionHandler handler = taskInfo[@"CompletionHandler"];
+    if (handler) {
+    handler(location, downloadTask.response, downloadTask.error);
+    }
+    
+    // update progress
+    if (!self.queueProgress.isCancelled) {
+    self.queueProgress.completedUnitCount++;
+    }
+    if (self.queueProgressHandler) {
+    self.queueProgressHandler(self.queueProgress);
+    }
+    
+    // queue has completed
+    if (self.queueCompletionHandler && self.queueProgress.fractionCompleted == 1.0) {
+    self.queueCompletionHandler();
+    }
+    }
+    }
+    
+    - (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
+    {
+    NSString *key = @(downloadTask.originalRequest.URL.absoluteString.hash).stringValue;
+    [_myLock lock];
+    NSDictionary *taskInfo = self.tasks[key];
+    [_myLock unlock];
+    if (taskInfo) {
+    ProgressHandler handler = taskInfo[@"ProgressHandler"];
+    if (handler) {
+    NSProgress *progress = [NSProgress progressWithTotalUnitCount:totalBytesExpectedToWrite];
+    progress.completedUnitCount = totalBytesWritten;
+    handler(progress);
+    }
+    }
     }
 }
